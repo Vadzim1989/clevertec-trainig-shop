0aa9fb094f893193d73155047a1a6cdd0d550717
Vadim Denisovski <vadim.denisovski@gmail.com>
2022-02-08 15:55:20 +0300

    lets start

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header_top">
+                <div className="header_top_info">
+                    <p><img src="" alt="" /></p>
+                    <p><img src="" alt="" /></p>
+                    <p><img src="" alt="" /></p>
+                </div>
+                <div className="header_top_social">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
